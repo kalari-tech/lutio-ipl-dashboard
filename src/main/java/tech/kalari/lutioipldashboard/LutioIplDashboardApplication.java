04cb717194bc21ef3a847d8c8f118c573819fd76
Rijo Joseph C <rijojosephc.1@gmail.com>
2021-10-19 19:15:23 +0530

    Step1: Initialized repo with Spring Initializr

---
+++ src/main/java/tech/kalari/lutioipldashboard/LutioIplDashboardApplication.java
@@ -0,0 +1,13 @@
+package tech.kalari.lutioipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutioIplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LutioIplDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/kalari/lutioipldashboard/LutioIplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kalari.lutioipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LutioIplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
